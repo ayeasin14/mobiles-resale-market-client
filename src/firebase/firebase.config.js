419f174b3c4ebd7902e8a781f343553d8965a0fc
ayeasin14 <ayeasin14@gmail.com>
2022-11-27 23:44:04 +0600

    dotenv file added

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,15 +5,18 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyCeTRwDpI_FqCHRsshLKnYKolbzEXSMxbw",
-    authDomain: "mobile-resale-market.firebaseapp.com",
-    projectId: "mobile-resale-market",
-    storageBucket: "mobile-resale-market.appspot.com",
-    messagingSenderId: "535610582597",
-    appId: "1:535610582597:web:409e738a481a3a8adea52c"
+    apiKey: process.env.REACT_APP_apiKey,
+    authDomain: process.env.REACT_APP_authDomain,
+    projectId: process.env.REACT_APP_projectId,
+    storageBucket: process.env.REACT_APP_storageBucket,
+    messagingSenderId: process.env.REACT_APP_messagingSenderId,
+    appId: process.env.REACT_APP_appId
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
 export default app;
+
+
+
