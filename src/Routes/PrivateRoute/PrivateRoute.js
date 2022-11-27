c5d2980f080ca0d40c5d4a53836ecdd316e1ab35
ayeasin14 <ayeasin14@gmail.com>
2022-11-27 23:14:28 +0600

    Admin and Privet routes added

---
+++ src/Routes/PrivateRoute/PrivateRoute.js
@@ -0,0 +1,23 @@
+import React, { useContext } from 'react';
+import { Navigate, useLocation } from 'react-router-dom';
+import { AuthContext } from '../../contexts/AuthProvider';
+
+const PrivateRoute = ({ children }) => {
+    const { user, loading } = useContext(AuthContext);
+    const location = useLocation();
+
+    if (loading) {
+        return <div className='text-center my-60'>
+            <p className='text-2xl text-primary'>Loading...</p>
+            <progress className="progress w-56"></progress>
+        </div>
+    }
+
+    if (user) {
+        return children;
+    }
+
+    return <Navigate to='/login' state={{ from: location }} replace></Navigate>;
+};
+
+export default PrivateRoute;
